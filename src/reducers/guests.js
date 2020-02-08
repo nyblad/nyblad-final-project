cd839369ba38b8800061d04deb20224202bb3275
nyblad <nyblad@hotmail.com>
2020-02-08 21:43:17 +0100

    Updated thunk to just 'json'

--- src/reducers/guests.js
+++ src/reducers/guests.js
@@ -21,7 +21,7 @@ export const fetchGuests = (path) => {
     fetch(`https://nyblad-guest-list.herokuapp.com${path}`)
       .then(res => res.json())
       .then(json => {
-        dispatch(guests.actions.setGuests(json.guests))
+        dispatch(guests.actions.setGuests(json))
         dispatch(ui.actions.setLoading(false))
       })
   }
